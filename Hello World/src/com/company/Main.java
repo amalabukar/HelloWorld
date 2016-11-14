bb9d8a1aeb5bcfb3d4d79141085e557e612b50cf
Amal Abukar <amalabukar@amals-mbp.apollosrc.root>
2016-11-14 11:27:48 -0600

    Hello World

---
+++ Hello World/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person p= new Person();
+        p.name= "Alice";
+        p.age= 30;
+        p.IsAlive= true;
+
+    }
+}
---
+++ Hello World/src/com/company/Person.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by amalabukar on 11/14/16.
+ */
+public class Person {
+    String name;
+    int age;
+    boolean isAlive;
+
+}
